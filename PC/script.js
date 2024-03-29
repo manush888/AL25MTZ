0e110fcefc4d99ee0d38c62078e139c2f6a62b98
M Sandesha 2006 <manushasandesha@gmail.com>
2024-03-29 23:14:15 +0530

    appearance improved

---
+++ PC/script.js
@@ -0,0 +1,16 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const fonts = ['raleway', 'pacifico', 'redditmono', 'rubikscribble', 'EduNSWACTFoundation'];
+    let currentIndex = 0;
+    const textElement = document.getElementById('cngtxt');
+
+    function changeFont() {
+        textElement.style.fontFamily = fonts[currentIndex];
+        currentIndex = (currentIndex + 1) % fonts.length;
+    }
+
+    changeFont();
+
+    setInterval(changeFont, 5000);
+});
